dc71ad4be1e422fa19c45ffa8ca601c15e7006c9
AsymmetricIris <42551549+AsymmetricIris@users.noreply.github.com>
2023-11-19 04:21:35 +0000

    Not resolved problems with servo control. Attempting to diagnose still. || TODO || - resolve servo ctrl jerkiness

--- arduino/pot_ctrl/pot_ctrl.ino
+++ arduino/pot_ctrl/pot_ctrl.ino
@@ -1,10 +1,13 @@
 #include <Servo.h>
 
+const int BAUD = 300;
+
 uint8_t rd_pins[] = {A0, A1, A2, A3, A4, A5, A6};
-uint8_t servo_ctrl_pins[] = {7, 3, 4, 5, 6, 8};         //TODO - make this progression linear
+uint8_t servo_ctrl_pins[] = {4, 5, 6, 7, 8, 0};         //TODO - make this progression linear
 Servo servos[] = {  Servo(), Servo(), Servo(), 
                     Servo(), Servo(), Servo()   };
-long joint_angles[] = {0, 0, 0, 0, 0, 0};               //will assign values to angles before we write ctrl signals
+// long joint_angles[] = {0, 0, 0, 0, 0, 0};
+long joint_angles[] = {0, 140, 160, 90, 40, 10};               //will assign values to angles before we write ctrl signals
 uint8_t servo_count = 1;                              //will use 6 in later versions using the RoboArm
 
 void assign_pins(Servo joint_servos[], uint8_t ctrl_pins[], uint8_t pin_count, long angles[]);
@@ -15,21 +18,14 @@ void print_angles(uint8_t pin_count, long angles[]);
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  assign_pins(servos, servo_ctrl_pins, servo_count, joint_angles);
+  Serial.begin(300);
+  // assign_pins(servos, servo_ctrl_pins, servo_count, joint_angles);
+  assign_pins(servos, servo_ctrl_pins, 6, joint_angles);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  long angle = map(sensorValue, 0, 1023, 0, 360);
-  // print out the value you read:
-  Serial.print(voltage);
-  Serial.print(" \t ");
-  rd_angles(servo_ctrl_pins, servo_count, joint_angles);
+  rd_angles(rd_pins, servo_count, joint_angles);
   print_angles(servo_count, joint_angles);
   wr_angles(servos, servo_count, joint_angles);
 }
@@ -39,10 +35,14 @@ void assign_pins(Servo joint_servos[], uint8_t ctrl_pins[], uint8_t pin_count, l
   //Assign pins & set servo angles to home position
   for (int8_t idx = 0; idx < pin_count; idx++)
   {
+    //set read pins
+    pinMode(rd_pins[idx], INPUT);
+
+    // pinMode(ctrl_pins[idx], OUTPUT);
     // Set default servo angles
     joint_servos[idx].write(angles[idx]);
     // Assign servo ctrl pins
-    joint_servos[idx].attach(ctrl_pins[idx]);
+    joint_servos[idx].attach(servo_ctrl_pins[idx]);
   }
 }
 
